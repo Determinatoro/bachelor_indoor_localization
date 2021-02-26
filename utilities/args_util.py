d2985ca0897c7ba4249a8e67bd9c3531d4b9855f
puchasa <aipsen18@student.aau.dk>
2021-02-26 15:43:10 +0100

    added comments

--- utilities/args_util.py
+++ utilities/args_util.py
@@ -2,6 +2,14 @@ from data_classes.args_data import ArgsData
 
 
 def parse_args(parameters):
+    """
+    parses the arguments supplied when executing the program
+    :param parameters: list of arguments supplied when executing
+    :return: object of type Args_Data consisting of
+        a) string with data root folder
+        b) list of strings with site id's (split at ",") if supplied
+        If no parameters are supplied it returns None
+    """
     arr_length = len(parameters)
     if arr_length == 0:
         return None
--- utilities/csv_util.py
+++ utilities/csv_util.py
@@ -1,18 +1,30 @@
 import csv
 import io
 from pathlib import Path
-
 from data_classes.phone_data import ReadData, Site, Dataset, BuildingPath
 
 
-def read_all_data_files(args_data):
+def read_all_data_files(args_data, max_path_files=None):
+    """
+    goes through all data in root folder EXCEPT when site id's are supplied
+    :param args_data: Of type ArgsData, from function parse_args
+    :return: Object of type DataSet consisting of
+        a) list of train data
+        b) list of test data
+    """
     test_data_path = args_data.root_data_path + "/test"
     train_data_path = args_data.root_data_path + "/train"
-    # returns a list of Path objects
-    test_path_filenames = get_files_in_folder(args_data.site_ids, test_data_path)[:1]
-    train_path_filenames = get_files_in_folder(args_data.site_ids, train_data_path)[:1]
 
-    # array of sites
+    # returns a list of Path objects
+    test_path_filenames = get_files_in_folder(args_data.site_ids, test_data_path)
+    train_path_filenames = get_files_in_folder(args_data.site_ids, train_data_path)
+
+    # if max_path_files is set only that amount of files are used further
+    if max_path_files is not None:
+        test_path_filenames = test_path_filenames[:max_path_files]
+        train_path_filenames = train_path_filenames[:max_path_files]
+
+    # lists of sites
     test_sites = get_sites(args_data, test_path_filenames)
     train_sites = get_sites(args_data, train_path_filenames)
 
@@ -20,49 +32,75 @@ def read_all_data_files(args_data):
 
 
 def get_files_in_folder(site_ids, data_path):
+    """
+    returns a list of csv file objects in folder data_path
+    :param site_ids: a list of site id's whose files to get
+    :param data_path: root data path
+    :return: list of file objects where each correspond to a path on a given site
+    """
+    # if no specific sites are supplied return a list of all files
     path_obj = Path(data_path).resolve()
     if len(site_ids) == 0:
         return list(path_obj.glob("*.csv"))
 
     file_paths = []
     for site_id in site_ids:
-        file_paths.extend(list(path_obj.glob(site_id + "*.csv")))
+        file_paths.extend(path_obj.glob(site_id + "*.csv"))
 
     return file_paths
 
 
 def get_sites(args_data, path_filenames):
+    """
+
+    :param args_data: data type ArgsData
+    :param path_filenames: list of path objects
+    :return: list of site objects
+    """
     temp_sites = []
     for file_path in path_filenames:
+        # filter out extension
         file_name = file_path.name.split(".")[0]
+        # returns list of strings separated by "_"
         split_file_name = file_name.split("_")
         length = len(split_file_name)
 
+        # default values
         floor_id = None
         path_id = None
         site_id = None
 
+        # identifies different parts of the name
+        # site_floor_path OR site_path --> site
         if length >= 1:
             site_id = split_file_name[0]
+        # site_path --> path
         if length == 2:
             path_id = split_file_name[1]
+        # site_floor_path --> floor AND path
         if length == 3:
             floor_id = floor_converter(split_file_name[1])
             path_id = split_file_name[2]
+        # stops if args_data has no site_id's OR the specific id is not in args_data
         if len(args_data.site_ids) > 0 and site_id not in args_data.site_ids:
             continue
 
+        # inserts site id in list sites IF site_id IS NOT already in temp_sites
         sites = list(filter(lambda x: x.site_id == site_id, temp_sites))
+        # if not in temp_sites make a Site object and add to temp_sites
         if len(sites) == 0:
             site = Site(site_id)
             temp_sites.append(site)
+        # if already in temp_sites
         else:
             site = sites[0]
 
+        # add information to path object
         path = BuildingPath(path_id)
         path.data = read_data_file(file_path)
         path.floor = floor_id
 
+        # adds paths to site object
         site.paths.append(path)
         print(file_path.name)
 
@@ -70,9 +108,17 @@ def get_sites(args_data, path_filenames):
 
 
 def floor_converter(floor_name):
+    """
+    edits floor_name to match convention
+    B1 --> -1 AND F1 --> 0
+    :param floor_name: string floor name from the middle of file name
+    :return: integer representing floor number
+    """
     floor_id = None
+    # removes F and decriments number
     if "F" in floor_name:
         floor_id = int(floor_name.replace("F", "")) - 1
+    # removes b and makes number negative
     if "B" in floor_name:
         floor_id = -int(floor_name.replace("B", ""))
 
@@ -80,6 +126,11 @@ def floor_converter(floor_name):
 
 
 def read_data_file(data_filename):
+    """
+    extracts data from csv file
+    :param data_filename: Path object
+    :return: an array of ReadData objects
+    """
     # Read lines from file
     with open(data_filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -170,16 +221,25 @@ def read_data_file(data_filename):
 
 
 def type_casting(dic, key, type):
+    """
+    type casts the value of dic[key] to type
+    :param dic:
+    :param key:
+    :param type:
+    :return: nothing. changes recorded in dic
+    """
     if key not in dic:
         dic[key] = None
         return
 
+    # get value of dic[key]
     value = dic.pop(key)
 
     if value == "" or value == "nan":
         dic[key] = None
         return
 
+    # sets type of value according to type parameter
     if type == int:
         dic[key] = int(value)
     elif type == float:
